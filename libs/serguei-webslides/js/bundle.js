87c40468dba3d9cb8cd18e36d1ca7c04c0c27248
englishextra <englishextra@yandex.ru>
2017-02-27 00:13:40 +0300

    u

--- libs/serguei-webslides/js/bundle.js
+++ libs/serguei-webslides/js/bundle.js
@@ -1,4 +1,141 @@
 /*!
+ * modified for babel ToProgress v0.1.1
+ * http://github.com/djyde/ToProgress
+ * gist.github.com/englishextra/6a8c79c9efbf1f2f50523d46a918b785
+ * jsfiddle.net/englishextra/z5xhjde8/
+ * arguments.callee changed to TP, a local wrapper function,
+ * so that public function name is now customizable;
+ * wrapped in curly brackets:
+ * else{document.body.appendChild(this.progressBar);};
+ * removed AMD, CommonJS support
+ * added window object existence check
+ * passes jshint
+ */
+var ToProgress=(function(){if("undefined"==typeof window||!("document"in window)){return console.log("window is undefined or document is not in window"),!1;}var TP=function(){function t(){var s=document.createElement("fakeelement"),i={transition:"transitionend",OTransition:"oTransitionEnd",MozTransition:"transitionend",WebkitTransition:"webkitTransitionEnd"};for(var j in i){if(i.hasOwnProperty(j)){if(void 0!==s.style[j]){return i[j];}}}}function s(t,a){if(this.progress=0,this.options={id:"top-progress-bar",color:"#F44336",height:"2px",duration:0.2},t&&"object"==typeof t){for(var i in t){if(t.hasOwnProperty(i)){this.options[i]=t[i];}}}if(this.options.opacityDuration=3*this.options.duration,this.progressBar=document.createElement("div"),this.progressBar.id=this.options.id,this.progressBar.setCSS=function(t){for(var a in t){if(t.hasOwnProperty(a)){this.style[a]=t[a];}}},this.progressBar.setCSS({position:a?"relative":"fixed",top:"0",left:"0",right:"0","background-color":this.options.color,height:this.options.height,width:"0%",transition:"width "+this.options.duration+"s, opacity "+this.options.opacityDuration+"s","-moz-transition":"width "+this.options.duration+"s, opacity "+this.options.opacityDuration+"s","-webkit-transition":"width "+this.options.duration+"s, opacity "+this.options.opacityDuration+"s"}),a){var o=document.querySelector(a);if(o){if(o.hasChildNodes()){o.insertBefore(this.progressBar,o.firstChild);}else{o.appendChild(this.progressBar);}}}else{document.body.appendChild(this.progressBar);}}var i=t();return s.prototype.transit=function(){this.progressBar.style.width=this.progress+"%";},s.prototype.getProgress=function(){return this.progress;},s.prototype.setProgress=function(t,s){this.show();this.progress=t>100?100:0>t?0:t;this.transit();if(s){s();}},s.prototype.increase=function(t,s){this.show();this.setProgress(this.progress+t,s);},s.prototype.decrease=function(t,s){this.show();this.setProgress(this.progress-t,s);},s.prototype.finish=function(t){var s=this;this.setProgress(100,t);this.hide();if(i){this.progressBar.addEventListener(i,function(t){s.reset();s.progressBar.removeEventListener(t.type,TP);});}},s.prototype.reset=function(t){this.progress=0;this.transit();if(t){t();}},s.prototype.hide=function(){this.progressBar.style.opacity="0";},s.prototype.show=function(){this.progressBar.style.opacity="1";},s;};return TP();}());
+/*!
+ * A function for elements selection - v0.1.9
+ * github.com/finom/bala
+ * @param {String} a id, class or tag string
+ * @param {String|Object} [b] context tag string or HTML Element object
+ * a=BALA("sometag/#someid/.someclass"[,someParent]);
+ * a=BALA.one("sometag/#someid/.someclass"[,someParent]);
+ * global $ becomes var g
+ * renamed function $ to g
+ * added window object existence check
+ * source: github.com/finom/bala/blob/master/bala.js
+ * passes jshint
+ */
+var BALA=(function(){if("undefined"==typeof window||!("document"in window)){return console.log("window is undefined or document is not in window"),!1;}var g=(function(document,s_addEventListener,s_querySelectorAll){function g(s,context,bala){bala=Object.create(g.fn);if(s){bala.push.apply(bala,s[s_addEventListener]?[s]:""+s===s?/</.test(s)?((context=document.createElement(context||s_addEventListener)).innerHTML=s,context.children):context?((context=g(context)[0])?context[s_querySelectorAll](s):bala):document[s_querySelectorAll](s):typeof s=='function'?document.readyState[7]?s():document[s_addEventListener]('DOMContentLoaded',s):s);}return bala;}g.fn=[];g.one=function(s,context){return g(s,context)[0]||null;};return g;})(document,'addEventListener','querySelectorAll');return g;}());
+/*!
+ * safe way to handle console.log():
+ * sitepoint.com/safe-console-log/
+ */
+/* jshint ignore:start */
+if ("undefined" === typeof console) {
+	console = {};
+	console.log = function () {
+		return;
+	};
+}
+/* jshint ignore:end */
+/*!
+ * modified MediaHack - (c) 2013 Pomke Nohkan MIT LICENCED.
+ * gist.github.com/englishextra/ff8c9dde94abe32a9d7c4a65e0f2ccac
+ * jsfiddle.net/englishextra/xg7ce8kc/
+ * removed className fallback and additionally
+ * returns earlyDeviceOrientation,earlyDeviceSize
+ * Add media query classes to DOM nodes
+ * github.com/pomke/mediahack/blob/master/mediahack.js
+ */
+var earlyDeviceOrientation="",earlyDeviceSize="";(function(w,e){var f=function(a){var b=a.split(" ");if(e){for(var c=0;c<b.length;c++){a=b[c];e.add(a);}}},g=function(a){var b=a.split(" ");if(e){for(var c=0;c<b.length;c++){a=b[c];e.remove(a);}}},h={landscape:"all and (orientation:landscape)",portrait:"all and (orientation:portrait)"},k={small:"all and (max-width:768px)",medium:"all and (min-width:768px) and (max-width:991px)",large:"all and (min-width:992px)"},d,mM="matchMedia",m="matches",o=function(a,b){var c=function(a){if(a[m]){f(b);earlyDeviceOrientation=b;}else{g(b);}};c(a);a.addListener(c);},s=function(a,b){var c=function(a){if(a[m]){f(b);earlyDeviceSize=b;}else{g(b);}};c(a);a.addListener(c);};for(d in h){if(h.hasOwnProperty(d)){o(w[mM](h[d]),d);}}for(d in k){if(k.hasOwnProperty(d)){s(w[mM](k[d]),d);}}})(window,document.documentElement.classList||"");
+/*!
+ * add mobile or desktop class
+ * using Detect Mobile Browsers | Open source mobile phone detection
+ * Regex updated: 1 August 2014
+ * detectmobilebrowsers.com
+ * github.com/heikojansen/plack-middleware-detectmobilebrowsers
+ */
+var earlyDeviceType="";(function(d,h,k,n){var c=/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(n)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(n.substr(0,4))?h:k;if(d&&c){d.classList.add(c);earlyDeviceType=c;}}(document.documentElement||"","mobile","desktop",navigator.userAgent||navigator.vendor||window.opera));
+/*!
+ * add svg support class
+ */
+var earlySvgSupport="";(function(d,s){var c=document.implementation.hasFeature("http://www.w3.org/2000/svg","1.1")?s:"no-"+s;(earlySvgSupport=c);if(d&&c){d.classList.add(c);}}(document.documentElement||"","svg"));
+/*!
+ * add svgasimg support class
+ */
+var earlySvgasimgSupport="";(function(d,s){var c=document.implementation.hasFeature("http://www.w3.org/TR/SVG11/feature#Image","1.1")?s:"no-"+s;(earlySvgasimgSupport=c);if(d&&c){d.classList.add(c);}}(document.documentElement||"","svgasimg"));
+/*!
+ * add touch support class
+ * gist.github.com/englishextra/3cb22aab31a52b6760b5921e4fe8db95
+ * jsfiddle.net/englishextra/z5xhjde8/
+ */
+var earlyHasTouch="";(function(d,s){if(d){var c="ontouchstart"in d?s:"no-"+s;earlyHasTouch=c;d.classList.add(c);}}(document.documentElement||"","touch"));
+/*!
+ * return date in YYYY-MM-DD format
+ */
+var earlyFnGetYyyymmdd=function(){"use strict";var a=(new Date()),b=a.getDate(),d=a.getFullYear(),c=a.getMonth();(c+=1);if(10>b){b="0"+b;}if(10>c){c="0"+c;}return d+"-"+c+"-"+b;}();
+/*!
+ * append details to title
+ */
+var initialDocumentTitle = document.title || "",
+userBrowsingDetails = " [" + (earlyFnGetYyyymmdd ? earlyFnGetYyyymmdd : "") + (earlyDeviceType ? " " + earlyDeviceType : "") + (earlyDeviceSize ? " " + earlyDeviceSize : "") + (earlyDeviceOrientation ? " " + earlyDeviceOrientation : "") + (earlySvgSupport ? " " + earlySvgSupport : "") + (earlySvgasimgSupport ? " " + earlySvgasimgSupport : "") + (earlyHasTouch ? " " + earlyHasTouch : "") + "]";
+if (document.title) {
+	document.title = document.title + userBrowsingDetails;
+}
+/*!
+ * modified for babel Evento - v1.0.0
+ * by Erik Royall <erikroyalL@hotmail.com> (http://erikroyall.github.io)
+ * Dual licensed under MIT and GPL
+ * identifier needs binding if its function has arguments
+ * var fn=function(a){a=a||"";if(a){return a;}};
+ * evento.add(window,"load",fn.bind(null,some_value));
+ * removed Array.prototype.indexOf shim
+ * removed Helio stuff which seems to be added
+ * for some other library, and works without Helio
+ * jsbin.com/jilevi/edit?html,js,output
+ * jsfiddle.net/englishextra/hLxyvmcm/
+ * exposed as window property
+ * added window object existence check
+ * source: gist.github.com/erikroyall/6618740
+ * source: gist.github.com/englishextra/3a959e4da0fcc268b140
+ * passes jshint
+ */
+var evento=(function(){return function(){if("undefined"==typeof window||!("document"in window)){return console.log("window is undefined or document is not in window"),!1;}var win=window,doc=win.document,_handlers={},addEvent,removeEvent,triggerEvent;addEvent=(function(){if(typeof doc.addEventListener==="function"){return function(el,evt,fn){el.addEventListener(evt,fn,false);_handlers[el]=_handlers[el]||{};_handlers[el][evt]=_handlers[el][evt]||[];_handlers[el][evt].push(fn);};}else if(typeof doc.attachEvent==="function"){return function(el,evt,fn){el.attachEvent(evt,fn);_handlers[el]=_handlers[el]||{};_handlers[el][evt]=_handlers[el][evt]||[];_handlers[el][evt].push(fn);};}else{return function(el,evt,fn){el["on"+evt]=fn;_handlers[el]=_handlers[el]||{};_handlers[el][evt]=_handlers[el][evt]||[];_handlers[el][evt].push(fn);};}}());removeEvent=(function(){if(typeof doc.removeEventListener==="function"){return function(el,evt,fn){el.removeEventListener(evt,fn,false);};}else if(typeof doc.detachEvent==="function"){return function(el,evt,fn){el.detachEvent(evt,fn);};}else{return function(el,evt,fn){el["on"+evt]=undefined;};}}());triggerEvent=function(el,evt){_handlers[el]=_handlers[el]||{};_handlers[el][evt]=_handlers[el][evt]||[];for(var _i=0,_l=_handlers[el][evt].length;_i<_l;_i+=1){_handlers[el][evt][_i]();}};return{add:addEvent,remove:removeEvent,trigger:triggerEvent,_handlers:_handlers};}();}());
+/*!
+ * set style opacity of an element
+ * @param {Object} a an HTML Element
+ * @param {Number} n any positive decimal number 0.00-1.00
+ * setStyleOpacity(a,n)
+ */
+var setStyleOpacity=function(a,n){n=n||1;return function(){if(a){a.style.opacity=n;}}();};
+/*!
+ * init ToProgress and extend methods
+ */
+var progressBar = new ToProgress({
+		id : "top-progress-bar",
+		color : "#FF5454",
+		height : "3px",
+		duration : 0.2
+	});
+/*!
+ * @memberof progressBar
+ * @param {Int} [n] a whole positive number
+ * progressBar.init(n)
+ */
+progressBar.init = function (n) {
+	n = n || 20;
+	return this.increase(n);
+};
+/*!
+ * @memberof progressBar
+ * progressBar.complete()
+ */
+progressBar.complete = function () {
+	return this.finish(),
+	this.hide();
+};
+progressBar.init();
+/*!
  * init webslides
  */
 var initWebslides = function() {
@@ -372,9 +509,9 @@ var initWebslides = function() {
 			/*!
 			 * show page
 			 */
-			$(".page").css({
+			/* $(".page").css({
 				opacity: 1
-			});
+			}); */
 		});
 		// Prototype better, faster. To show the grid/baseline.png, press Enter on keyboard
 		$(document).keypress(function (e) {
@@ -387,3 +524,14 @@ var initWebslides = function() {
 };
 /* loadJS("//cdn.jsdelivr.net/jquery/3.1.1/jquery.min.js", initWebslides); */
 loadJS("../libs/serguei-webslides/js/vendors.min.js", initWebslides);
+/*!
+ * show page, finish ToProgress
+ */
+var showPageFinishProgress = function () {
+	"use strict";
+	var a = BALA.one("#page") || "";
+	console.log("triggered function: showPageFinishProgress");
+	setStyleOpacity(a, 1);
+	progressBar.complete();
+};
+evento.add(window, "load", showPageFinishProgress);
