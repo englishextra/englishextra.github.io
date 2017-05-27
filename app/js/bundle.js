f872db060189a9506807517328a25c24167a95c1
englishextra <englishextra@yandex.ru>
2017-05-27 22:13:13 +0300

    updates

--- app/js/bundle.js
+++ app/js/bundle.js
@@ -64,10 +64,10 @@ if ("undefined" === typeof console) {
  * @see {@link https://github.com/squeral/lightbox}
  * @see {@link https://github.com/squeral/lightbox/blob/master/lightbox.js}
  * @params {Object} elem Node element
- * new HDLightbox(elem)
+ * new IframeLightbox(elem)
  * passes jshint
  */
-(function(root){"use strict";var HDLightbox=function(elem){this.trigger=elem;this.el=document.querySelector(".iframe-lightbox");this.body=document.querySelector(".iframe-lightbox .body");this.content=document.querySelector(".iframe-lightbox .content");this.href=elem.dataset.src||"";this.paddingBottom=elem.dataset.paddingBottom||"";this.image=null;this.video=null;this.init();};HDLightbox.prototype.init=function(){var _this=this;if(!this.el)this.create();this.trigger.addEventListener("click",function(e){e.preventDefault();_this.open();});};HDLightbox.prototype.create=function(){var _this=this,bd=document.createElement("div");this.el=document.createElement("div");this.content=document.createElement("div");this.body=document.createElement("div");this.el.classList.add("iframe-lightbox");bd.classList.add("backdrop");this.content.classList.add("content");this.body.classList.add("body");this.el.appendChild(bd);this.content.appendChild(this.body);this.content_holder=document.createElement("div");this.content_holder.classList.add("content-holder");this.content_holder.appendChild(this.content);this.el.appendChild(this.content_holder);document.body.appendChild(this.el);bd.addEventListener("click",function(){_this.close();});var f=function(e){if(_this.isOpen())return;_this.el.classList.remove("show");_this.body.innerHTML="";};this.el.addEventListener("transitionend",f,false);this.el.addEventListener("webkitTransitionEnd",f,false);this.el.addEventListener("mozTransitionEnd",f,false);this.el.addEventListener("msTransitionEnd",f,false);};HDLightbox.prototype.loadIframe=function(){this.body.innerHTML='<iframe src="'+this.href+'" name="iframe-lightbox-'+Date.now()+'" onload="this.style.opacity=1;" style="opacity:0;border:none;" scrolling="no" webkitallowfullscreen="true" mozallowfullscreen="true" allowfullscreen="true" height="166" frameborder="no"></iframe>';};HDLightbox.prototype.open=function(){this.loadIframe();if(this.paddingBottom){this.content.style.paddingBottom=this.paddingBottom;}else{this.content.removeAttribute("style");}this.el.classList.add("show");this.el.classList.add("open");};HDLightbox.prototype.close=function(){this.el.classList.remove("open");};HDLightbox.prototype.isOpen=function(){return this.el.classList.contains("open");};root.HDLightbox=HDLightbox;})(globalRoot);
+(function(root){"use strict";var d=document,aEL="addEventListener",gEBI="getElementById",gEBCN="getElementsByClassName",cE="createElement",cL="classList",aC="appendChild",dS="dataset",containerClass="iframe-lightbox",isLoadedClass="is-loaded",isOpenedClass="is-opened",isShowingClass="is-showing";var IframeLightbox=function(elem){this.trigger=elem;this.el=d[gEBCN](containerClass)[0]||"";this.body=this.el?this.el[gEBCN]("body")[0]:"";this.content=this.el?this.el[gEBCN]("content")[0]:"";this.href=elem[dS].src||"";this.paddingBottom=elem[dS].paddingBottom||"";this.init();};IframeLightbox.prototype.init=function(){var _this=this;if(!this.el)this.create();this.trigger[aEL]("click",function(e){e.preventDefault();_this.open();});};IframeLightbox.prototype.create=function(){var _this=this,bd=d[cE]("div");this.el=d[cE]("div");this.content=d[cE]("div");this.body=d[cE]("div");this.el[cL].add(containerClass);bd[cL].add("backdrop");this.content[cL].add("content");this.body[cL].add("body");this.el[aC](bd);this.content[aC](this.body);this.content_holder=d[cE]("div");this.content_holder[cL].add("content-holder");this.content_holder[aC](this.content);this.el[aC](this.content_holder);d.body[aC](this.el);bd[aEL]("click",function(){_this.close();});var f=function(e){if(_this.isOpen())return;_this.el[cL].remove(isShowingClass);_this.body.innerHTML="";};this.el[aEL]("transitionend",f,false);this.el[aEL]("webkitTransitionEnd",f,false);this.el[aEL]("mozTransitionEnd",f,false);this.el[aEL]("msTransitionEnd",f,false);};IframeLightbox.prototype.loadIframe=function(){this.iframeId=containerClass+Date.now();this.body.innerHTML='<iframe src="'+this.href+'" name="'+this.iframeId+'" id="'+this.iframeId+'" onload="this.style.opacity=1;" style="opacity:0;border:none;" scrolling="no" webkitallowfullscreen="true" mozallowfullscreen="true" allowfullscreen="true" height="166" frameborder="no"></iframe>';(function(iframeId,body){d[gEBI](iframeId).onload=function(){this.style.opacity=1;body[cL].add(isLoadedClass);};})(this.iframeId,this.body);};IframeLightbox.prototype.open=function(){this.loadIframe();if(this.paddingBottom){this.content.style.paddingBottom=this.paddingBottom;}else{this.content.removeAttribute("style");}this.el[cL].add(isShowingClass);this.el[cL].add(isOpenedClass);};IframeLightbox.prototype.close=function(){this.el[cL].remove(isOpenedClass);this.body[cL].remove(isLoadedClass);};IframeLightbox.prototype.isOpen=function(){return this.el[cL].contains(isOpenedClass);};root.IframeLightbox=IframeLightbox;})(globalRoot);
 /*!
  * modified scrollToY
  * @see {@link http://stackoverflow.com/questions/8917921/cross-browser-javascript-not-jquery-scroll-to-top-animation}
@@ -681,7 +681,7 @@ var manageIframeLightboxLinks = function (ctx) {
 	isBindedClass = "is-binded",
 	arrangeDataSrcIframe = function (e) {
 		if (!e[cL].contains(isBindedClass)) {
-			e.lightbox = new HDLightbox(e);
+			e.lightbox = new IframeLightbox(e);
 			e[cL].add(isBindedClass);
 		}
 	},
@@ -929,6 +929,7 @@ manageOtherDropdownLists = function () {
 	}
 };
 document.ready().then(manageOtherDropdownLists);
+globalRoot.addEventListener("hashchange", handleOtherDropdownLists);
 /*!
  * add smooth scroll or redirection to static select options
  * @param {Object} [ctx] context HTML Element
@@ -1772,10 +1773,7 @@ var manageLocationQrCodeImage = function () {
 	if (btn && holder && locationHash) {
 		if ("undefined" !== typeof getHTTP && getHTTP()) {
 			/* console.log("triggered function: manageLocationQrCodeImage"); */
-			var handleLocationQrCodeIsActiveClass = function () {
-				holder[cL].remove(isActiveClass);
-			},
-			generateLocationQrCodeImg = function () {
+			var generateLocationQrCodeImg = function () {
 				var newText = w.location.href || "",
 				newImg = d[cE]("img"),
 				newTitle = d.title ? ("Ссылка на страницу «" + d.title.replace(/\[[^\]]*?\]/g, "").trim() + "»") : "",
@@ -1830,8 +1828,6 @@ var manageLocationQrCodeImage = function () {
 			};
 			btn[aEL]("click", generateLocationQrCodeImg);
 			btn[aEL]("click", handleLocationQrCodeButton);
-			/* w[aEL]("hashchange", generateLocationQrCodeImg); */
-			w[aEL]("hashchange", handleLocationQrCodeIsActiveClass);
 		}
 	}
 };
@@ -1960,6 +1956,7 @@ manageOtherSocialButtons = function () {
 	}
 };
 document.ready().then(manageOtherSocialButtons);
+globalRoot.addEventListener("hashchange", handleOtherSocialButtons);
 /*!
  * init col debug btn
  */
