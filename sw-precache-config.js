86af555db08bd202d9f2ce22bc07d79118ec8d60
englishextra <englishextra@yandex.ru>
2017-06-05 21:30:07 +0300

    updated service-worker.js

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -1,77 +1,78 @@
 /*!
  * @see {@link https://github.com/GoogleChrome/sw-precache/issues/97}
  * @see {@link https://github.com/GoogleChrome/sw-precache#runtime-caching}
+ * @see {@link https://developers.google.com/web/ilt/pwa/using-sw-precache-and-sw-toolbox}
  */
 module.exports = {
-	staticFileGlobs: ['index.html',
-		'manifest.json',
-		'yandex-tableau.json',
-		'**.{png,ico,svg}',
-		'cdn/**/*.{png,jpg,js,json,css}',
-		'fonts/**/*.{eot,ttf,woff,woff2}',
-		'libs/**/img/**/*.{png,jpg}',
-		'pages/**/*.html'],
-	stripPrefix: './',
+	staticFileGlobs: ["index.html",
+		"manifest.json",
+		"yandex-tableau.json",
+		"**.{png,ico,svg}",
+		"cdn/**/*.{png,jpg,js,json,css}",
+		"fonts/**/*.{eot,ttf,woff,woff2}",
+		"libs/**/img/**/*.{png,jpg}",
+		"pages/**/*.html"],
+	stripPrefix: "./",
 	runtimeCaching: [{
 			urlPattern: /^https:\/\/mc\.yandex\.ru/,
-			handler: 'networkOnly'/* ,
+			handler: "networkOnly",
 			options: {
 				debug: true
-			} */
+			}
 		}, {
 			urlPattern: /^https:\/\/www\.google-analytics\.com/,
-			handler: 'networkOnly'/* ,
+			handler: "networkOnly",
 			options: {
 				debug: true
-			} */
+			}
 		}, {
 			urlPattern: /^https:\/\/ssl\.google-analytics\.com/,
-			handler: 'networkOnly'/* ,
+			handler: "networkOnly",
 			options: {
 				debug: true
-			} */
+			}
 		}, {
 			urlPattern: /^https:\/\/(.*?)\.disqus\.com/,
-			handler: 'networkOnly'/* ,
+			handler: "networkOnly",
 			options: {
 				debug: true
-			} */
+			}
 		}, {
 			urlPattern: /^https:\/\/w\.soundcloud\.com/,
-			handler: 'networkOnly'/* ,
+			handler: "networkOnly",
 			options: {
 				debug: true
-			} */
+			}
 		}, {
 			urlPattern: /^https:\/\/player\.vimeo\.com/,
-			handler: 'networkOnly'/* ,
+			handler: "networkOnly",
 			options: {
 				debug: true
-			} */
+			}
 		}, {
 			urlPattern: /^https:\/\/www\.youtube\.com/,
-			handler: 'networkOnly'/* ,
+			handler: "networkOnly",
 			options: {
 				debug: true
-			} */
+			}
 		}, {
 			urlPattern: /\/libs\/(.*?)\/css\//,
-			handler: 'fastest'/* ,
+			handler: "fastest",
 			options: {
 				debug: true
-			} */
+			}
 		}, {
 			urlPattern: /\/libs\/(.*?)\/js\//,
-			handler: 'fastest'/* ,
+			handler: "fastest",
 			options: {
 				debug: true
-			} */
+			}
 		}, {
 			urlPattern: /\/libs\/(.*?)\/json\//,
-			handler: 'fastest'/* ,
+			handler: "fastest",
 			options: {
 				debug: true
-			} */
+			}
 		}
 	]
 };
