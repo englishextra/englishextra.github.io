8636fe03442e37f5b00d98799a1c18aec325e70e
englishextra <englishextra@yandex.ru>
2017-08-16 00:14:46 +0300

    updates

--- libs/sitemap/js/bundle.js
+++ libs/sitemap/js/bundle.js
@@ -124,6 +124,119 @@ var globalRoot = "undefined" !== typeof window ? window : this;
 	}();root.ToProgress = ToProgress;
 })(globalRoot);
 /*!
+ * modified verge 1.9.1+201402130803
+ * @see {@link https://github.com/ryanve/verge}
+ * MIT License 2013 Ryan Van Etten
+ * removed module
+ * converted to dot notation
+ * added &&r.left<=viewportW()&&(0!==el.offsetHeight);
+ * added &&r.left<=viewportW()&&(0!==el.offsetHeight);
+ * added &&r.top<=viewportH()&&(0!==el.offsetHeight);
+ * Substitute inViewport with: inY on vertical sites, inX on horizontal ones.
+ * On pages without horizontal scroll, inX is always true.
+ * On pages without vertical scroll, inY is always true.
+ * If the viewport width is >= the document width, then inX is always true.
+ * bug: inViewport returns true if element is hidden
+ * @see {@link https://github.com/ryanve/verge/issues/19}
+ * @see {@link https://github.com/ryanve/verge/blob/master/verge.js}
+ * passes jshint
+ */
+(function (root) {
+	"use strict";
+	var verge = function () {
+		var xports = {},
+		    win = typeof root !== "undefined" && root,
+		    doc = typeof document !== "undefined" && document,
+		    docElem = doc && doc.documentElement,
+		    matchMedia = win.matchMedia || win.msMatchMedia,
+		    mq = matchMedia ? function (q) {
+			return !!matchMedia.call(win, q).matches;
+		} : function () {
+			return false;
+		},
+		    viewportW = xports.viewportW = function () {
+			var a = docElem.clientWidth,
+			    b = win.innerWidth;return a < b ? b : a;
+		},
+		    viewportH = xports.viewportH = function () {
+			var a = docElem.clientHeight,
+			    b = win.innerHeight;return a < b ? b : a;
+		};xports.mq = mq;xports.matchMedia = matchMedia ? function () {
+			return matchMedia.apply(win, arguments);
+		} : function () {
+			return {};
+		};function viewport() {
+			return { "width": viewportW(), "height": viewportH() };
+		}xports.viewport = viewport;xports.scrollX = function () {
+			return win.pageXOffset || docElem.scrollLeft;
+		};xports.scrollY = function () {
+			return win.pageYOffset || docElem.scrollTop;
+		};function calibrate(coords, cushion) {
+			var o = {};cushion = +cushion || 0;o.width = (o.right = coords.right + cushion) - (o.left = coords.left - cushion);o.height = (o.bottom = coords.bottom + cushion) - (o.top = coords.top - cushion);return o;
+		}function rectangle(el, cushion) {
+			el = el && !el.nodeType ? el[0] : el;if (!el || 1 !== el.nodeType) {
+				return false;
+			}return calibrate(el.getBoundingClientRect(), cushion);
+		}xports.rectangle = rectangle;function aspect(o) {
+			o = null === o ? viewport() : 1 === o.nodeType ? rectangle(o) : o;var h = o.height,
+			    w = o.width;h = typeof h === "function" ? h.call(o) : h;w = typeof w === "function" ? w.call(o) : w;return w / h;
+		}xports.aspect = aspect;xports.inX = function (el, cushion) {
+			var r = rectangle(el, cushion);return !!r && r.right >= 0 && r.left <= viewportW() && 0 !== el.offsetHeight;
+		};xports.inY = function (el, cushion) {
+			var r = rectangle(el, cushion);return !!r && r.bottom >= 0 && r.top <= viewportH() && 0 !== el.offsetHeight;
+		};xports.inViewport = function (el, cushion) {
+			var r = rectangle(el, cushion);return !!r && r.bottom >= 0 && r.right >= 0 && r.top <= viewportH() && r.left <= viewportW() && 0 !== el.offsetHeight;
+		};return xports;
+	}();root.verge = verge;
+})(globalRoot);
+/*!
+ * return image is loaded promise
+ * @see {@link https://jsfiddle.net/englishextra/56pavv7d/}
+ * @param {String|Object} s image path string or HTML DOM Image Object
+ * var m = document.querySelector("img") || "";
+ * var s = m.src || "";
+ * imagePromise(m).then(function (r) {
+ * alert(r);
+ * }).catch (function (err) {
+ * alert(err);
+ * });
+ * imagePromise(s).then(function (r) {
+ * alert(r);
+ * }).catch (function (err) {
+ * alert(err);
+ * });
+ * @see {@link https://gist.github.com/englishextra/3e95d301d1d47fe6e26e3be198f0675e}
+ * passes jshint
+ */
+(function (root) {
+	"use strict";
+	var imagePromise = function (s) {
+		if (root.Promise) {
+			return new Promise(function (y, n) {
+				var f = function (e, p) {
+					e.onload = function () {
+						y(p);
+					};e.onerror = function () {
+						n(p);
+					};e.src = p;
+				};if ("string" === typeof s) {
+					var a = new Image();f(a, s);
+				} else {
+					if ("img" !== s.tagName) {
+						return Promise.reject();
+					} else {
+						if (s.src) {
+							f(s, s.src);
+						}
+					}
+				}
+			});
+		} else {
+			throw new Error("Promise is not in global object");
+		}
+	};globalRoot.imagePromise = imagePromise;
+})(globalRoot);
+/*!
  * modified scrollToY
  * @see {@link http://stackoverflow.com/questions/8917921/cross-browser-javascript-not-jquery-scroll-to-top-animation}
  * passes jshint
@@ -866,6 +979,83 @@ var handleExternalLink = function (url, ev) {
 };
 document.ready().then(manageExternalLinks);
 /*!
+ * replace img src with data-src
+ * @param {Object} [ctx] context HTML Element
+ */
+var handleDataSrcImages = function () {
+	"use strict";
+
+	var d = document,
+	    gEBCN = "getElementsByClassName",
+	    cL = "classList",
+	    ds = "dataset",
+	    imgClass = "data-src-img",
+	    img = d[gEBCN](imgClass) || "",
+	    isActiveClass = "is-active",
+	    isBindedClass = "is-binded",
+	    rerenderDataSrcImage = function (e) {
+		if (!e[cL].contains(isBindedClass)) {
+			var srcString = e[ds].src || "";
+			if (srcString) {
+				if (parseLink(srcString).isAbsolute && !parseLink(srcString).hasHTTP) {
+					e[ds].src = srcString.replace(/^/, getHTTP(true) + ":");
+					srcString = e[ds].src;
+				}
+				imagePromise(srcString).then(function (r) {
+					e.src = srcString;
+					/* console.log("manageDataSrcImages => imagePromise: loaded image:", r); */
+				}).catch(function (err) {
+					/* console.log("manageDataSrcImages => imagePromise: cannot load image:", err); */
+				});
+				/* e.src = srcString; */
+				e[cL].add(isActiveClass);
+				e[cL].add(isBindedClass);
+			}
+		}
+	},
+	    arrangeDataSrcImage = function (e) {
+		/*!
+   * true if elem is in same y-axis as the viewport or within 100px of it
+   * @see {@link https://github.com/ryanve/verge}
+   */
+		if (verge.inY(e, 100) /*  && 0 !== e.offsetHeight */) {
+				rerenderDataSrcImage(e);
+			}
+	},
+	    arrangeAllDataSrcImages = function () {
+		for (var i = 0, l = img.length; i < l; i += 1) {
+			arrangeDataSrcImage(img[i]);
+		}
+		/* forEach(img, arrangeDataSrcImage, false); */
+	};
+	if (img) {
+		/* console.log("triggered function: manageDataSrcImages"); */
+		arrangeAllDataSrcImages();
+	}
+},
+    handleDataSrcImagesWindow = function () {
+	var throttleHandleDataSrcImages = throttle(handleDataSrcImages, 100);
+	throttleHandleDataSrcImages();
+},
+    manageDataSrcImages = function () {
+	"use strict";
+
+	var w = globalRoot,
+	    aEL = "addEventListener",
+	    rEL = "removeEventListener";
+	w[rEL]("scroll", handleDataSrcImagesWindow);
+	w[rEL]("resize", handleDataSrcImagesWindow);
+	w[aEL]("scroll", handleDataSrcImagesWindow);
+	w[aEL]("resize", handleDataSrcImagesWindow);
+	var timers = new Timers();
+	timers.timeout(function () {
+		timers.clear();
+		timers = null;
+		handleDataSrcImages();
+	}, 100);
+};
+document.ready().then(manageDataSrcImages);
+/*!
  * init masonry
  */
 var initMasonry = function () {
@@ -1121,11 +1311,11 @@ var showPageFinishProgress = function () {
 	};
 	if (container) {
 		/* console.log("triggered function: showPageFinishProgress"); */
-		if ("undefined" !== typeof imagesPreloaded) {
-			showContainerOnImagesPreloaded();
-		} else {
-			showContainer();
-		}
+		/* if ("undefined" !== typeof imagesPreloaded) {
+  	showContainerOnImagesPreloaded();
+  } else { */
+		showContainer();
+		/* } */
 	}
 };
 document.ready().then(showPageFinishProgress);
