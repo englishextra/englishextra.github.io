493c3b5634091a6a3ae78f09061c38a69e7aae68
englishextra <englishextra@yandex.ru>
2017-09-18 00:27:26 +0300

    updates

--- app/libs/pwa-englishextra/js/bundle.js
+++ app/libs/pwa-englishextra/js/bundle.js
@@ -1279,7 +1279,7 @@ var handleDataSrcImageAll = function () {
 	    rEL = "removeEventListener";
 	w[rEL]("scroll", handleDataSrcImageAllWindow);
 	w[rEL]("resize", handleDataSrcImageAllWindow);
-	w[aEL]("scroll", handleDataSrcImageAllWindow);
+	w[aEL]("scroll", handleDataSrcImageAllWindow, { passive: true });
 	w[aEL]("resize", handleDataSrcImageAllWindow);
 	var timers = new Timers();
 	timers.timeout(function () {
@@ -1352,7 +1352,7 @@ var handleDataSrcIframeAll = function () {
 	    rEL = "removeEventListener";
 	w[rEL]("scroll", handleDataSrcIframeAllWindow);
 	w[rEL]("resize", handleDataSrcIframeAllWindow);
-	w[aEL]("scroll", handleDataSrcIframeAllWindow);
+	w[aEL]("scroll", handleDataSrcIframeAllWindow, { passive: true });
 	w[aEL]("resize", handleDataSrcIframeAllWindow);
 	var timers = new Timers();
 	timers.timeout(function () {
@@ -3327,7 +3327,7 @@ var initUiTotop = function () {
 	if (b) {
 		/* console.log("triggered function: initUiTotop"); */
 		anchor[aEL]("click", handleUiTotopAnchor);
-		w[aEL]("scroll", handleUiTotopWindow);
+		w[aEL]("scroll", handleUiTotopWindow, { passive: true });
 	}
 };
 document.ready().then(initUiTotop);
--- app/libs/pwa-englishextra/src/bundle.js
+++ app/libs/pwa-englishextra/src/bundle.js
@@ -570,7 +570,7 @@ manageDataSrcImageAll = function () {
 	rEL = "removeEventListener";
 	w[rEL]("scroll", handleDataSrcImageAllWindow);
 	w[rEL]("resize", handleDataSrcImageAllWindow);
-	w[aEL]("scroll", handleDataSrcImageAllWindow);
+	w[aEL]("scroll", handleDataSrcImageAllWindow, {passive: true});
 	w[aEL]("resize", handleDataSrcImageAllWindow);
 	var timers = new Timers();
 	timers.timeout(function () {
@@ -641,7 +641,7 @@ manageDataSrcIframeAll = function (ctx) {
 	rEL = "removeEventListener";
 	w[rEL]("scroll", handleDataSrcIframeAllWindow);
 	w[rEL]("resize", handleDataSrcIframeAllWindow);
-	w[aEL]("scroll", handleDataSrcIframeAllWindow);
+	w[aEL]("scroll", handleDataSrcIframeAllWindow, {passive: true});
 	w[aEL]("resize", handleDataSrcIframeAllWindow);
 	var timers = new Timers();
 	timers.timeout(function () {
@@ -2586,7 +2586,7 @@ var initUiTotop = function () {
 	if (b) {
 		/* console.log("triggered function: initUiTotop"); */
 		anchor[aEL]("click", handleUiTotopAnchor);
-		w[aEL]("scroll", handleUiTotopWindow);
+		w[aEL]("scroll", handleUiTotopWindow, {passive: true});
 	}
 };
 document.ready().then(initUiTotop);
