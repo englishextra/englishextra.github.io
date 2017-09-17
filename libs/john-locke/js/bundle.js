9abe9e0b25c7f92e43c0c8321f479806857dfdb6
englishextra <englishextra@yandex.ru>
2017-09-17 23:23:10 +0300

    updates

--- libs/john-locke/js/bundle.js
+++ libs/john-locke/js/bundle.js
@@ -47,92 +47,28 @@
  * removed module check
  * passes jshint
  */
-(function (root) {
+//(function(root){"use strict";var ToProgress=(function(){var TP=function(){var t=function(){var s=document.createElement("fakeelement"),i={transition:"transitionend",OTransition:"oTransitionEnd",MozTransition:"transitionend",WebkitTransition:"webkitTransitionEnd"};for(var j in i){if(i.hasOwnProperty(j)){if(void 0!==s.style[j]){return i[j];}}}},s=function(t,a){if(this.progress=0,this.options={id:"top-progress-bar",color:"#F44336",height:"2px",duration:0.2},t&&"object"===typeof t){for(var i in t){if(t.hasOwnProperty(i)){this.options[i]=t[i];}}}if(this.options.opacityDuration=3*this.options.duration,this.progressBar=document.createElement("div"),this.progressBar.id=this.options.id,this.progressBar.setCSS=function(t){for(var a in t){if(t.hasOwnProperty(a)){this.style[a]=t[a];}}},this.progressBar.setCSS({position:a?"relative":"fixed",top:"0",left:"0",right:"0","background-color":this.options.color,height:this.options.height,width:"0%",transition:"width "+this.options.duration+"s, opacity "+this.options.opacityDuration+"s","-moz-transition":"width "+this.options.duration+"s, opacity "+this.options.opacityDuration+"s","-webkit-transition":"width "+this.options.duration+"s, opacity "+this.options.opacityDuration+"s"}),a){var o=document.querySelector(a);if(o){if(o.hasChildNodes()){o.insertBefore(this.progressBar,o.firstChild);}else{o.appendChild(this.progressBar);}}}else{document.body.appendChild(this.progressBar);}},i=t();return s.prototype.transit=function(){this.progressBar.style.width=this.progress+"%";},s.prototype.getProgress=function(){return this.progress;},s.prototype.setProgress=function(t,s){this.show();this.progress=t>100?100:0>t?0:t;this.transit();if(s){s();}},s.prototype.increase=function(t,s){this.show();this.setProgress(this.progress+t,s);},s.prototype.decrease=function(t,s){this.show();this.setProgress(this.progress-t,s);},s.prototype.finish=function(t){var s=this;this.setProgress(100,t);this.hide();if(i){this.progressBar.addEventListener(i,function(t){s.reset();s.progressBar.removeEventListener(t.type,TP);});}},s.prototype.reset=function(t){this.progress=0;this.transit();if(t){t();}},s.prototype.hide=function(){this.progressBar.style.opacity="0";},s.prototype.show=function(){this.progressBar.style.opacity="1";},s;};return TP();}());root.ToProgress=ToProgress;}("undefined" !== typeof window ? window : this,document));
+/*!
+ * start progress indicator
+ */
+/* (function (root) {
 	"use strict";
-	var ToProgress = function () {
-		var TP = function () {
-			var t = function () {
-				var s = document.createElement("fakeelement"),
-				    i = { transition: "transitionend", OTransition: "oTransitionEnd", MozTransition: "transitionend", WebkitTransition: "webkitTransitionEnd" };for (var j in i) {
-					if (i.hasOwnProperty(j)) {
-						if (void 0 !== s.style[j]) {
-							return i[j];
-						}
-					}
-				}
-			},
-			    s = function (t, a) {
-				if (this.progress = 0, this.options = { id: "top-progress-bar", color: "#F44336", height: "2px", duration: 0.2 }, t && "object" === typeof t) {
-					for (var i in t) {
-						if (t.hasOwnProperty(i)) {
-							this.options[i] = t[i];
-						}
-					}
-				}if (this.options.opacityDuration = 3 * this.options.duration, this.progressBar = document.createElement("div"), this.progressBar.id = this.options.id, this.progressBar.setCSS = function (t) {
-					for (var a in t) {
-						if (t.hasOwnProperty(a)) {
-							this.style[a] = t[a];
-						}
-					}
-				}, this.progressBar.setCSS({ position: a ? "relative" : "fixed", top: "0", left: "0", right: "0", "background-color": this.options.color, height: this.options.height, width: "0%", transition: "width " + this.options.duration + "s, opacity " + this.options.opacityDuration + "s", "-moz-transition": "width " + this.options.duration + "s, opacity " + this.options.opacityDuration + "s", "-webkit-transition": "width " + this.options.duration + "s, opacity " + this.options.opacityDuration + "s" }), a) {
-					var o = document.querySelector(a);if (o) {
-						if (o.hasChildNodes()) {
-							o.insertBefore(this.progressBar, o.firstChild);
-						} else {
-							o.appendChild(this.progressBar);
-						}
-					}
-				} else {
-					document.body.appendChild(this.progressBar);
-				}
-			},
-			    i = t();return s.prototype.transit = function () {
-				this.progressBar.style.width = this.progress + "%";
-			}, s.prototype.getProgress = function () {
-				return this.progress;
-			}, s.prototype.setProgress = function (t, s) {
-				this.show();this.progress = t > 100 ? 100 : 0 > t ? 0 : t;this.transit();if (s) {
-					s();
-				}
-			}, s.prototype.increase = function (t, s) {
-				this.show();this.setProgress(this.progress + t, s);
-			}, s.prototype.decrease = function (t, s) {
-				this.show();this.setProgress(this.progress - t, s);
-			}, s.prototype.finish = function (t) {
-				var s = this;this.setProgress(100, t);this.hide();if (i) {
-					this.progressBar.addEventListener(i, function (t) {
-						s.reset();s.progressBar.removeEventListener(t.type, TP);
-					});
-				}
-			}, s.prototype.reset = function (t) {
-				this.progress = 0;this.transit();if (t) {
-					t();
-				}
-			}, s.prototype.hide = function () {
-				this.progressBar.style.opacity = "0";
-			}, s.prototype.show = function () {
-				this.progressBar.style.opacity = "1";
-			}, s;
-		};return TP();
-	}();root.ToProgress = ToProgress;
-})("undefined" !== typeof window ? window : this, document);
-(function (root) {
-	"use strict";
-
 	root.progressBar = new ToProgress({
-		id: "top-progress-bar",
-		color: "#FF2C40",
-		height: "3px",
-		duration: 0.2
-	});
+			id: "top-progress-bar",
+			color: "#FF2C40",
+			height: "0.200rem",
+			duration: 0.2
+		});
 	root.progressBar.increase(20);
 	root.hideProgressBar = function () {
 		root.progressBar.finish();
 		root.progressBar.hide();
 	};
 	root.addEventListener("load", hideProgressBar);
-})("undefined" !== typeof window ? window : this);
+}
+	("undefined" !== typeof window ? window : this)); */
 /*!
+ * check for passive support
  * @see {@link https://github.com/WICG/EventListenerOptions/blob/gh-pages/explainer.md#feature-detection}
  */
 (function (root) {
@@ -180,7 +116,7 @@
 				ripple[cL].add("bounceOutUp");
 			}
 			timer3 = setTimeout(removeRipple, 5000);
-			progressBar.increase(20);
+			/* progressBar.increase(20); */
 		}
 	};
 	if ("undefined" !== typeof imagesPreloaded) {
@@ -193,7 +129,7 @@
 	var run = function () {
 		var cE = "createElement";
 		var aC = "appendChild";
-		progressBar.increase(20);
+		/* progressBar.increase(20); */
 		var qrcode = document[gEBCN]("qrcode")[0] || "";
 		var timer;
 		var showQrcode = function () {
--- libs/john-locke/src/bundle.js
+++ libs/john-locke/src/bundle.js
@@ -16,13 +16,16 @@
  * removed module check
  * passes jshint
  */
-(function(root){"use strict";var ToProgress=(function(){var TP=function(){var t=function(){var s=document.createElement("fakeelement"),i={transition:"transitionend",OTransition:"oTransitionEnd",MozTransition:"transitionend",WebkitTransition:"webkitTransitionEnd"};for(var j in i){if(i.hasOwnProperty(j)){if(void 0!==s.style[j]){return i[j];}}}},s=function(t,a){if(this.progress=0,this.options={id:"top-progress-bar",color:"#F44336",height:"2px",duration:0.2},t&&"object"===typeof t){for(var i in t){if(t.hasOwnProperty(i)){this.options[i]=t[i];}}}if(this.options.opacityDuration=3*this.options.duration,this.progressBar=document.createElement("div"),this.progressBar.id=this.options.id,this.progressBar.setCSS=function(t){for(var a in t){if(t.hasOwnProperty(a)){this.style[a]=t[a];}}},this.progressBar.setCSS({position:a?"relative":"fixed",top:"0",left:"0",right:"0","background-color":this.options.color,height:this.options.height,width:"0%",transition:"width "+this.options.duration+"s, opacity "+this.options.opacityDuration+"s","-moz-transition":"width "+this.options.duration+"s, opacity "+this.options.opacityDuration+"s","-webkit-transition":"width "+this.options.duration+"s, opacity "+this.options.opacityDuration+"s"}),a){var o=document.querySelector(a);if(o){if(o.hasChildNodes()){o.insertBefore(this.progressBar,o.firstChild);}else{o.appendChild(this.progressBar);}}}else{document.body.appendChild(this.progressBar);}},i=t();return s.prototype.transit=function(){this.progressBar.style.width=this.progress+"%";},s.prototype.getProgress=function(){return this.progress;},s.prototype.setProgress=function(t,s){this.show();this.progress=t>100?100:0>t?0:t;this.transit();if(s){s();}},s.prototype.increase=function(t,s){this.show();this.setProgress(this.progress+t,s);},s.prototype.decrease=function(t,s){this.show();this.setProgress(this.progress-t,s);},s.prototype.finish=function(t){var s=this;this.setProgress(100,t);this.hide();if(i){this.progressBar.addEventListener(i,function(t){s.reset();s.progressBar.removeEventListener(t.type,TP);});}},s.prototype.reset=function(t){this.progress=0;this.transit();if(t){t();}},s.prototype.hide=function(){this.progressBar.style.opacity="0";},s.prototype.show=function(){this.progressBar.style.opacity="1";},s;};return TP();}());root.ToProgress=ToProgress;}("undefined" !== typeof window ? window : this,document));
-(function (root) {
+//(function(root){"use strict";var ToProgress=(function(){var TP=function(){var t=function(){var s=document.createElement("fakeelement"),i={transition:"transitionend",OTransition:"oTransitionEnd",MozTransition:"transitionend",WebkitTransition:"webkitTransitionEnd"};for(var j in i){if(i.hasOwnProperty(j)){if(void 0!==s.style[j]){return i[j];}}}},s=function(t,a){if(this.progress=0,this.options={id:"top-progress-bar",color:"#F44336",height:"2px",duration:0.2},t&&"object"===typeof t){for(var i in t){if(t.hasOwnProperty(i)){this.options[i]=t[i];}}}if(this.options.opacityDuration=3*this.options.duration,this.progressBar=document.createElement("div"),this.progressBar.id=this.options.id,this.progressBar.setCSS=function(t){for(var a in t){if(t.hasOwnProperty(a)){this.style[a]=t[a];}}},this.progressBar.setCSS({position:a?"relative":"fixed",top:"0",left:"0",right:"0","background-color":this.options.color,height:this.options.height,width:"0%",transition:"width "+this.options.duration+"s, opacity "+this.options.opacityDuration+"s","-moz-transition":"width "+this.options.duration+"s, opacity "+this.options.opacityDuration+"s","-webkit-transition":"width "+this.options.duration+"s, opacity "+this.options.opacityDuration+"s"}),a){var o=document.querySelector(a);if(o){if(o.hasChildNodes()){o.insertBefore(this.progressBar,o.firstChild);}else{o.appendChild(this.progressBar);}}}else{document.body.appendChild(this.progressBar);}},i=t();return s.prototype.transit=function(){this.progressBar.style.width=this.progress+"%";},s.prototype.getProgress=function(){return this.progress;},s.prototype.setProgress=function(t,s){this.show();this.progress=t>100?100:0>t?0:t;this.transit();if(s){s();}},s.prototype.increase=function(t,s){this.show();this.setProgress(this.progress+t,s);},s.prototype.decrease=function(t,s){this.show();this.setProgress(this.progress-t,s);},s.prototype.finish=function(t){var s=this;this.setProgress(100,t);this.hide();if(i){this.progressBar.addEventListener(i,function(t){s.reset();s.progressBar.removeEventListener(t.type,TP);});}},s.prototype.reset=function(t){this.progress=0;this.transit();if(t){t();}},s.prototype.hide=function(){this.progressBar.style.opacity="0";},s.prototype.show=function(){this.progressBar.style.opacity="1";},s;};return TP();}());root.ToProgress=ToProgress;}("undefined" !== typeof window ? window : this,document));
+/*!
+ * start progress indicator
+ */
+/* (function (root) {
 	"use strict";
 	root.progressBar = new ToProgress({
 			id: "top-progress-bar",
 			color: "#FF2C40",
-			height: "3px",
+			height: "0.200rem",
 			duration: 0.2
 		});
 	root.progressBar.increase(20);
@@ -32,8 +35,9 @@
 	};
 	root.addEventListener("load", hideProgressBar);
 }
-	("undefined" !== typeof window ? window : this));
+	("undefined" !== typeof window ? window : this)); */
 /*!
+ * check for passive support
  * @see {@link https://github.com/WICG/EventListenerOptions/blob/gh-pages/explainer.md#feature-detection}
  */
 (function (root) {
@@ -80,7 +84,7 @@
 				ripple[cL].add("bounceOutUp");
 			}
 			timer3 = setTimeout(removeRipple, 5000);
-			progressBar.increase(20);
+			/* progressBar.increase(20); */
 		}
 	};
 	if ("undefined" !== typeof imagesPreloaded) {
@@ -93,7 +97,7 @@
 	var run = function () {
 		var cE = "createElement";
 		var aC = "appendChild";
-		progressBar.increase(20);
+		/* progressBar.increase(20); */
 		var qrcode = document[gEBCN]("qrcode")[0] || "";
 		var timer;
 		var showQrcode = function () {
