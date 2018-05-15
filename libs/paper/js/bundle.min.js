28baad781ea7076bbb72c19be84512b3c7b044d2
englishextra <englishextra@yandex.ru>
2018-05-15 22:42:16 +0300

    updates

--- libs/paper/js/bundle.min.js
+++ libs/paper/js/bundle.min.js
@@ -2087,11 +2087,12 @@ ToProgress, unescape, verge, VK, Ya*/
 		initUiTotop();
 
 		hideProgressBar();
-		
+
 		scroll2Top(0, 20000);
 	};
 
-	var scripts = ["../../libs/paper/css/bundle.min.css"];
+	/* var scripts = ["../../libs/paper/css/bundle.min.css"]; */
+	var scripts = [];
 
 	var supportsPassive = (function () {
 		var support = false;
@@ -2187,7 +2188,8 @@ ToProgress, unescape, verge, VK, Ya*/
 	};
 
 	loadCSS(
-			forcedHTTP + "://fonts.googleapis.com/css?family=Roboto:300,400,400i,700,700i%7CRoboto+Mono:400,700&subset=cyrillic,latin-ext",
+			/* forcedHTTP + "://fonts.googleapis.com/css?family=Roboto:300,400,400i,700,700i%7CRoboto+Mono:400,700&subset=cyrillic,latin-ext", */
+			"../../libs/paper/css/bundle.min.css",
 			onFontsLoadedCallback
 		);
 
