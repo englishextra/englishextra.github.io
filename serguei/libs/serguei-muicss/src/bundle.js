9ba163848b95284314734528a3573a43be9b5fa0
englishextra <englishextra@yandex.ru>
2018-06-14 18:49:47 +0300

    updates

--- serguei/libs/serguei-muicss/src/bundle.js
+++ serguei/libs/serguei-muicss/src/bundle.js
@@ -1912,7 +1912,7 @@ unescape, verge, VK, WheelIndicator, Ya*/
 				var svg = document[createElementNS]("http://www.w3.org/2000/svg", "svg");
 				var use = document[createElementNS]("http://www.w3.org/2000/svg", "use");
 				svg[setAttribute]("class", "ui-icon");
-				use[setAttributeNS]("http://www.w3.org/1999/xlink", "xlink:href", "#ui-icon-arrow-up");
+				use[setAttributeNS]("http://www.w3.org/1999/xlink", "xlink:href", "#ui-icon-outline-arrow_upward");
 				svg[appendChild](use);
 				targetObj[appendChild](svg);
 			};
