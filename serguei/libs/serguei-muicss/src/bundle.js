a04e0eaba7f176dcd5d58a12dfc46db16cb6f000
englishextra <englishextra@yandex.ru>
2018-09-24 14:08:04 +0300

    updates

--- serguei/libs/serguei-muicss/src/bundle.js
+++ serguei/libs/serguei-muicss/src/bundle.js
@@ -2265,17 +2265,11 @@ QRCode, require, ripple, t, twttr, unescape, VK, WheelIndicator, Ya*/
 	};
 
 	/* var scripts = [
-		"../../fonts/material-design-icons/3.0.1/css/material-icons.css",
-		"../../fonts/MaterialDesign-Webfont/2.2.43/css/materialdesignicons.css",
-		"../../fonts/roboto-fontfacekit/2.137/css/roboto.css",
-		"../../fonts/roboto-mono-fontfacekit/2.0.986/css/roboto-mono.css",
-		"./node_modules/normalize.css/normalize.css",
-		"../../cdn/highlight.js/9.12.0/css/hljs.css",
-		"./bower_components/iframe-lightbox/iframe-lightbox.css",
-				//"../../cdn/iframe-lightbox/0.1.7/css/iframe-lightbox.fixed.css",
-		"./bower_components/img-lightbox/img-lightbox.css",
-				//"../../cdn/img-lightbox/0.1.2/css/img-lightbox.fixed.css",
-		"./bower_components/mui/src/sass/mui.css"
+				"../../fonts/roboto-fontfacekit/2.137/css/roboto.css",
+				"../../fonts/roboto-mono-fontfacekit/2.0.986/css/roboto-mono.css",
+				"./bower_components/mui/src/sass/mui.css",
+				"./bower_components/iframe-lightbox/iframe-lightbox.css",
+				"./bower_components/img-lightbox/img-lightbox.css"
 	]; */
 
 	var scripts = [
@@ -2328,24 +2322,18 @@ QRCode, require, ripple, t, twttr, unescape, VK, WheelIndicator, Ya*/
 	}
 
 	/* var scripts = [
-		"./node_modules/jquery/dist/jquery.js",
-		"./bower_components/mui/packages/cdn/js/mui.js",
-		"./bower_components/iframe-lightbox/iframe-lightbox.js",
-				//"../../cdn/iframe-lightbox/0.1.7/js/iframe-lightbox.fixed.js",
-		"./bower_components/img-lightbox/img-lightbox.js",
-				//"../../cdn/img-lightbox/0.1.2/js/img-lightbox.fixed.js",
-		"./bower_components/qrjs2/qrjs2.js",
-				//"../../cdn/qrjs2/0.1.6/js/qrjs2.fixed.js",
-		"./bower_components/wheel-indicator/lib/wheel-indicator.js",
-		"./bower_components/verge/verge.js",
-		"./bower_components/Tocca.js/Tocca.js",
-		"../../cdn/t.js/0.1.0/js/t.fixed.js",
-		"./node_modules/mustache/mustache.js",
-				//"../../cdn/mustache/2.3.0/js/mustache.fixed.js",
-		"../../cdn/highlight.js/9.12.0/js/highlight.pack.fixed.js",
-		"../../cdn/verge/1.9.1/js/verge.fixed.js",
-		"../../cdn/Tocca.js/2.0.1/js/Tocca.fixed.js",
-		"../../cdn/wheel-indicator/1.1.4/js/wheel-indicator.fixed.js"
+				"./bower_components/iframe-lightbox/iframe-lightbox.js",
+				"./bower_components/img-lightbox/img-lightbox.js",
+				"./bower_components/qrjs2/qrjs2.js",
+				"./bower_components/Tocca.js/Tocca.js",
+				"./bower_components/wheel-indicator/lib/wheel-indicator.js",
+				"./node_modules/any-resize-event/dist/any-resize-event.js",
+				"./node_modules/macy/dist/macy.js",
+				"./node_modules/mustache/mustache.js",
+				"./node_modules/wolfy87-eventemitter/EventEmitter.js",
+				"../../cdn/minigrid/3.1.1/js/minigrid.fixed.js",
+				"../../cdn/ripple-js/1.4.4/js/ripple.fixed.js",
+				"../../cdn/ReadMore.js/1.0.0/js/readMoreJS.fixed.js"
 	]; */
 
 	scripts.push("./libs/serguei-muicss/js/vendors.min.js");
