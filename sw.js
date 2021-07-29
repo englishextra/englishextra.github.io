51d0072b22578004f88085f8c497a9b4f9e39381
englishextra <englishextra@yandex.ru>
2021-07-30 02:51:11 +0300

    navigation changes

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
  * https://github.com/meanbee/magento-meanbee-pwa/issues/20#issuecomment-497626347
  * This is a reworked code of the "Offline copy of pages" service worker
  */
-var cacheName = "shimansky.biz-offline-v1602190307";
+var cacheName = "shimansky.biz-offline-v1602190308";
 var indexPage = "index.html";
 var offlinePages = [indexPage];
 
